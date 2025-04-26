2094c3d3f8e9eaaa7f9fec97471df823e0c91e16
simoncastellanos2025 <simon.c.azure@gmail.com>
2025-04-26 12:08:15 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "c5941903-69ad-4ba1-b201-6f86d9a24226"
-  client_id       = "your-client-id"
+  client_id       = "03122896-ae63-471a-a602-2aabf042a881"
   client_secret   = "your-client-secret"
   tenant_id       = "ac5b3248-01a9-4eb0-bdf4-e89223653965"
   #skip_provider_registration = true  # Add this line
